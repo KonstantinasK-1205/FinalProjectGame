42168366b66e90205d5066156874c8d4164f7a3b
Evaldas Granickas <evaldasgr@gmail.com>
2022-12-07 15:41:20 +0200

    Added healthpacks again

--- sound.py
+++ sound.py
@@ -13,5 +13,8 @@ class Sound:
 
         self.hpHealed = pg.mixer.Sound(self.path + 'hpHealed.wav')
         self.dmgIncrease = pg.mixer.Sound(self.path + 'dmgIncrease.wav')
+
+        self.healthpack = pg.mixer.Sound(self.path + 'healthpack.wav')
+
         # self.theme = pg.mixer.Sound(self.path + 'theme.wav')
 
